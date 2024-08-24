88a0fd6dbb9a5674bafe84d596a899d7838803a5
Yaroslav Dudar <y.dudar@list.ru>
2024-08-24 15:12:38 +0300

    add project

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./dist/**/*.{html,js}",
+  ],
+  theme: {
+    screens: {
+      "tablet": "375px",
+      // => @media (min-width: 375px) { ... }
+    },
+    colors: {
+      'dark-cyan': 'hsl(158 36% 37%)',
+      'cyan': 'hsl(158, 44%, 20%)',
+      'cream': 'hsl(30 38% 92%)',
+      'very-dark-blue': 'hsl(212, 21% 14%)',
+      'dark-grayish-blue': 'hsl(228 12% 48%)',
+      'white': 'hsl(0 0% 100%)',
+    },
+    fontFamily: {
+      Fraunces: ["Fraunces", 'serif'],
+      Montserrat: ["Montserrat", 'sans-serif'],
+
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
